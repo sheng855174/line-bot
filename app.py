d51cdf850c3bed8fbfaaac5955b378e8584f99c7
sheng855174 <sheng855174@gmail.com>
2019-09-14 01:59:38 +0800

    init commit

--- app.py
+++ app.py
@@ -46,13 +46,14 @@ def handle_message(event):
         if group_id == "C193ba92879d441b6a12a533a18be62a9":
             fp = open("member","w+")
             fp.write("24128 二兵 戴立宸 0920108958 未回報\n24129 二兵 林翰霳 0939085913\n24130 二兵 魏廷宇 0963085102\n24131 二兵 薛博尹 0989997805\n24132 二兵 劉　煒 0966087873\n24133 二兵 陳嘉宏 0936742136\n24134 二兵 張秉禎 0930265781\n24135 二兵 蘇弘碩 0975507212\n24136 二兵 陳昇昇 0932023953\n24137 二兵 周川景 0973282283\n24138 二兵 翁浩哲 0972143417\n24139 二兵 胡皓翔 0973767218\n24140 二兵 陳芳順 0963053651\n24141 二兵 許家瑋 0988218690\n")
+            text += "過"
             fp.close()
-            fp = open("member","rw")
+            '''fp = open("member","rw")
             line = fp.readline()
             while line:
                 text += line
                 line = fp.readline()
-            fp.close()
+            fp.close()'''
     message = TextSendMessage(text)
     line_bot_api.reply_message(event.reply_token, message)
 
