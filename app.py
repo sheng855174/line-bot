38228d6fb57a9632bd7520738cb7666146c6a7bd
sheng855174 <sheng855174@gmail.com>
2019-09-14 00:51:44 +0800

    init commit

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def callback():
 def handle_message(event):
     user_id = event.source.user_id
     profile = line_bot_api.get_profile(user_id)
-    text = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n"
-    + profile.display_name
+    text = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n" + 
+    profile.display_name
     message = TextSendMessage(text)
     line_bot_api.reply_message(event.reply_token, message)
 
