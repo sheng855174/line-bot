948bac9c3e14e5645dee76080174445fa1f868a6
sheng855174 <sheng855174@gmail.com>
2019-09-13 20:59:46 +0800

    test hello

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    text=event.message.text
+    test="hello"
+    message = TextSendMessage(text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
