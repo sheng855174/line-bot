61ae92f5d38031caf0de4b73b98d983cca097310
sheng855174 <sheng855174@gmail.com>
2019-09-15 09:02:52 +0800

    init

--- app_run.py
+++ app_run.py
@@ -13,12 +13,10 @@ def index():
     history_dic = {}
     history_list = []
     for _data in data_UserData:
-        history_dic['Id'] = _data.Id
-        history_dic['Level'] = _data.Level
         history_dic['Name'] = _data.Name
-        history_dic['SmartPhone'] = _data.SmartPhone
-        history_dic['Time'] = _data.Time
+        history_dic['Id'] = _data.Id
         history_dic['Description'] = _data.Description
+        history_dic['CreateDate'] = _data.CreateDate.strftime('%Y/%m/%d %H:%M:%S')
         history_list.append(history_dic)
         history_dic = {}
     return render_template('index.html', **locals())
@@ -26,20 +24,13 @@ def index():
 
 @app.route('/API/add_data', methods=['POST'])
 def add_data():
-    id = request.form['Id']
-    level = request.form['Level']
-    name = request.form['Name']
-    smartPhone = request.form['SmartPhone']
-    time = request.form['Time']
-    description = request.form['Description']
+    name = request.form['name']
+    description = request.form['description']
     if name != "" and description != "":
         add_data = UserData(
-            Id=id,
-            Level=level,
             Name=name,
-            SmartPhone=smartPhone,
-            Time=time,
             Description=description,
+            CreateDate=datetime.now()
         )
         db.session.add(add_data)
         db.session.commit()
--- dbModel.py
+++ dbModel.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'postgres://kstpgaxwunspii:39ba2c30de5fc7ebe972edd6e8ba6fdcdab1373d923f14189dee6ebc040aaaa5@ec2-174-129-227-146.compute-1.amazonaws.com:5432/d7r6b8n61fqmdg'
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://ajwybovxjkrkfe:fd35b0570d9d12a1300b106d26e656aaff93de0b9c17d13db37ebb289396ddd5@ec2-50-19-221-38.compute-1.amazonaws.com:5432/d8o90kcva1q2lj'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -15,26 +15,21 @@ manager.add_command('db', MigrateCommand)
 
 
 class UserData(db.Model):
-    __tablename__ = 'MemberData'
+    __tablename__ = 'UserData'
+
     Id = db.Column(db.Integer, primary_key=True)
-    Level = db.Column(db.String(64))
     Name = db.Column(db.String(64))
-    SmartPhone = db.Column(db.String(64))
-    Time = db.Column(db.String(64))
-    Description = db.Column(db.String(512))
+    Description = db.Column(db.String(256))
+    CreateDate = db.Column(db.DateTime)
 
     def __init__(self
-                 , Level
                  , Name
-                 , SmartPhone
-                 , Time
                  , Description
+                 , CreateDate
                  ):
-        self.Level = Level
         self.Name = Name
-        self.SmartPhone = SmartPhone
-        self.Time = Time
         self.Description = Description
+        self.CreateDate = CreateDate
 
 
 if __name__ == '__main__':
